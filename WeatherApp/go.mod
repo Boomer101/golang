a1f441624e723ea97b07a45a9e70eedce731fb9b
Adrian Bachmann <boomerhimself@gmx.ch>
2022-12-29 19:23:02 +0100

    Add config.yaml, rename to getRandomWeather

--- WeatherApp/go.mod
+++ WeatherApp/go.mod
@@ -1,4 +1,5 @@
 module weatherapp
 
-
 go 1.19
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- WeatherApp/main.go
+++ WeatherApp/main.go
@@ -2,25 +2,58 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 	"weatherapp/weatherdata"
+
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	http.HandleFunc("/hello", getWeather)
-	log.Fatal(http.ListenAndServe(":8090", nil))
+
+	config, err := getConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := config.Server.Host + ":" + config.Server.Port
+	fmt.Printf("Starting server at: %v", addr)
+
+	http.HandleFunc("/getweather", getRandomWeather)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func getWeather(w http.ResponseWriter, req *http.Request) {
+func getConfig() (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open("config.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func getRandomWeather(w http.ResponseWriter, request *http.Request) {
 
 	summaries := [...]string{"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"}
+	summariesCount := int32(len(summaries))
 
 	randomTemp := (rand.Int31n(50-(-20)) - 20)
 
-	randomPicker := rand.Int31n(9-1) + 1
+	randomPicker := rand.Int31n(summariesCount-1) + 1
 	randomSummary := summaries[randomPicker]
 
 	responseData := weatherdata.WeatherData{Date: time.Now().Format("2 Jan 2006"), TempCelsius: randomTemp, Summary: randomSummary}
